9a57d48369a947f3d6464a9f4f318dedf8656815
Ansh Kevadiya <anshkevadiyaextra@gmail.com>
2024-02-24 01:29:42 +0530

    add the api function in the views

--- coinmasters/views.py
+++ coinmasters/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render,redirect
+import requests
 
 
 
 
 
 def function(request):
-    return render(request,'index.html')
+    api_url = 'https://applicationsjson.s3.amazonaws.com/coin_master/coin_master.json'
+    response = requests.get(api_url).json()
+    # keys = [item['key'] for item in api_url]
+    context = {'apidata': response}
+    return render(request,'index.html',context)
+
+
